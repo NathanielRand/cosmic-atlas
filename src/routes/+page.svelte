3032095309fe1734d7e19356c4a767ade62a7984
Nathaniel Rand <nathanieljrand@gmail.com>
2024-05-09 17:40:44 -0400

    Add home page hero sample

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Section, News, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
+	import { Button } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+	// Image 
+	import hero_planet from '$lib/assets/images/mars.jpg'
+  </script>
+  
+<Section>
+	<div class="w-full">
+	  	<img src={hero_planet} alt="hero planet" />
+	</div>
+</Section>
